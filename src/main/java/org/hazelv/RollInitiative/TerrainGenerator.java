2cb78ab2987cf48bdc72a23544fb30191d515484
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-06-03 19:03:44 -0600

    Tried to do 'gradient trick,' failed.

--- src/main/java/org/hazelv/RollInitiative/TerrainGenerator.java
+++ src/main/java/org/hazelv/RollInitiative/TerrainGenerator.java
@@ -3,11 +3,9 @@ package org.hazelv.RollInitiative;
 import de.articdive.jnoise.core.api.functions.Interpolation;
 import de.articdive.jnoise.generators.noise_parameters.fade_functions.FadeFunction;
 import de.articdive.jnoise.pipeline.JNoise;
-import net.minestom.server.coordinate.BlockVec;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.instance.generator.GenerationUnit;
 import net.minestom.server.instance.generator.Generator;
 import org.jetbrains.annotations.NotNull;
@@ -19,12 +17,14 @@ import static java.lang.Math.*;
 public class TerrainGenerator implements Generator {
     final long seed;
     final JNoise noise;
+    public InstanceContainer parent;
+    //change these later based on choice.
     final int octaves = 8;
     final int lacunarity = 3;
     final double persistence = 0.3;
     final int scale = 640;
     final int height_scale = 384;
-    public InstanceContainer parent;
+    final double waterHeight = 80;
 
     public TerrainGenerator(long seed, InstanceContainer parent) {
         this.seed = seed;
@@ -41,8 +41,8 @@ public class TerrainGenerator implements Generator {
                 Point bottom = start.add(x, 0, z);
                 double height = (fractalPerlin(bottom.x(), bottom.z()) * height_scale) + 64;
                 generationUnit.modifier().fill(bottom, bottom.add(1, 0, 1).withY(height), Block.STONE);
-                if (80 >= bottom.y() && 80 <= generationUnit.absoluteEnd().y() && height < 80) {
-                    generationUnit.modifier().fill(bottom.add(1, height + 1, 1), bottom.add(1, 80, 1), Block.OBSIDIAN);
+                if (waterHeight >= bottom.y() && waterHeight <= generationUnit.absoluteEnd().y() && height < waterHeight) {
+                    generationUnit.modifier().fill(bottom.withY(height + 1), bottom.add(1, waterHeight, 1), Block.WATER);
                 }
             }
         }
@@ -59,7 +59,8 @@ public class TerrainGenerator implements Generator {
         double z1 = z;
         double amplitude = 1;
         for (int i = 0; i < octaves; i++) {
-            value += abs((noise.evaluateNoise(x1 / scale, z1 / scale) * amplitude));
+            double noiseAt = abs((noise.evaluateNoise(x1 / scale, z1 / scale) * amplitude));
+            value += noiseAt;
             //value += (noise.evaluateNoise(x1 / scale, z1 / scale) * amplitude);
             x1 *= lacunarity;
             z1 *= lacunarity;
